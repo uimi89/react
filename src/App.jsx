a33c0e2fa30eca4b79ec6a8560b08554fbe0d716
Irving Hans <uimi89@hotmail.com>
2025-03-21 13:49:40 -0700

    Primer commit. Versión inicial del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import { MultiplesEffect } from './Hooks/useEffect/MultiplesEffect'
 import { UseEffect } from './Hooks/useEffect/UseEffect'
 
 function App() {
 
   return (
     <div className="App">
-       <UseEffect/>
+      <MultiplesEffect/>
     </div>
   )
 }
